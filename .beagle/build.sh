47a4645ac6b28a460301568b1cbb4c1d3600a33e
shucheng <shucheng@bd-apaas.com>
2022-10-12 22:45:03 +0800

    v0.4.5 fix Makefile

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-export ARTIFACT_PLATFORMS="linux-amd64 linux-arm64 linux-ppc64le linux-mips64le"
+# export ARTIFACT_PLATFORMS="linux-amd64 linux-arm64 linux-ppc64le linux-mips64le"
+# export TEST_PLATFORMS="linux/amd64,linux/arm64,linux/ppc64le,linux/mips64le"
 
 make artifacts
